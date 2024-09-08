52c9594f73b109e29c8abb6a527c58f8f3192c8f
Shivam <txt2shivam@gmail.com>
2024-09-08 19:25:23 +0530

    Scroll To Top componenet added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import FetchNews from "./Components/FetchNews"
 import Header from "./Components/Navbar";
 import Footer from './Components/Footer';
 import Hero from './Components/Hero';
+import ScrollToTop from './Components/ScrollToTop';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <BrowserRouter>
         <Header /> 
         <Hero/>
+        <ScrollToTop/>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/category/*" element={<FetchNews />} />
