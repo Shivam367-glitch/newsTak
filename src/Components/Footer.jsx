e3cc2526566cf0bdda96d998f9f85b4bde356468
Shivam <txt2shivam@gmail.com>
2024-09-08 12:15:57 +0530

    footer modified

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,17 +1,16 @@
 
 import React from 'react'
 
-import { FaLinkedin, FaTwitter, FaInstagram, FaFacebook, FaYoutube } from "react-icons/fa";
+import { FaLinkedin, FaTwitter, FaGithub,FaEnvelope} from "react-icons/fa";
 function Footer() {
   return (
-    <div className='container-fluid p-3 d-flex justify-content-center align-items-center flex-column footer_container ' style={{ height: "25vh" }}>
+    <div className='container-fluid p-3 d-flex justify-content-center align-items-center flex-column footer_container ' >
       <h1>Find Me On</h1>
       <div className='list-group list-group-horizontal' style={{ listStyleType: "none" }}>
         <a className='list-group-item' href="https://www.linkedin.com/in/shivam-mishra-8aba7b179/" target="_blank"> <FaLinkedin /></a>
-        <a className='list-group-item' href="https://twitter.com/Shivamm3213" target={"_blank"} ><FaTwitter /></a>
-        <a className='list-group-item' href="https://www.facebook.com/profile.php?id=100011628730183" target="_blank" ><FaFacebook /></a>
-        <a className='list-group-item' href="https://www.instagram.com/txt2shivam/?hl=en" target="_blank" ><FaInstagram /></a>
-        <a className='list-group-item' href="https://www.youtube.com/channel/UCv8mLTYO-LiH1yhsh8lnAZg" target="_blank" ><FaYoutube /></a>
+        <a className='list-group-item' href="https://twitter.com/Shivamm3213" target="_blank" ><FaTwitter /></a>
+        <a className='list-group-item' href="http://github.com/Shivam367-glitch/" target="_blank" ><FaGithub /></a>
+        <a className='list-group-item' href="mailto:txt2shivam@gmail.com" target="_blank" ><FaEnvelope/></a>
       </div>
     </div>
   )
